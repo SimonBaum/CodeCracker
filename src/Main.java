72998dc20693467e0d98316440a522dcb71c6e9e
SimonBaum <s-sbaum1@haw-landshut.de>
2022-11-15 21:45:23 +0100

    added some basic functions to main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+// program to solve a vignere chiffre with a known password length
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int passwordLength;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter password length:");
+        passwordLength = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Enter encrypted text:");
+        String encText = sc.nextLine().toUpperCase() ;
+        encText = encText.replaceAll("[^a-zA-Z]", "").trim();
+
+        System.out.println(encText);
+
+        sc.close();
     }
+
+
 }
